bf66760930bfecae4aca301e2d067df0b6e409a7
Kingshuk Bose <kingshuk.bose91@live.com>
2021-06-30 20:28:33 +0530

    Complex number init

---
+++ arithmetic/complex.py
@@ -0,0 +1,8 @@
+# Declaration of complex number and Addition,Substraction,Multipication and Division  
+class Complex:
+    def __init__(self,real,imaginary,polar=False):
+        self.real=real
+        self.imaginary=imaginary
+
+    
+
